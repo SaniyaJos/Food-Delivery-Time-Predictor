7f2b2d70830fd52515ea74f26718127c83e252bb
SaniyaJos <sanjo2027@gmail.com>
2026-01-10 14:34:03 +0530

    modified code in app.py for deployment

--- model/train.py
+++ model/train.py
@@ -23,7 +23,7 @@ y = df_encoded["Delivery_Time_min"]
 
 
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=70
+    X, y, test_size=0.2, random_state=42
 )
 
 
